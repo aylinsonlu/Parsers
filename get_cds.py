82f0452709564a1cd8eca48c68476f04c4491477
Aylin <aylinsonlu@gmail.com>
2022-06-09 14:47:16 +0300

    Checks cds gene_bio_type is protein coding

--- get_cds.py
+++ get_cds.py
@@ -19,24 +19,26 @@ def get_all_isoform_cds(gtf_file,genome_file):
                     start = fields[3]
                     stop = fields[4]
                     transcript_id = attributes.split("transcript_id ")[1].split(";")[0]
-                    if strand=="-":                            
-                        output = os.popen("samtools faidx " + genome_file + " " +chrm + ":" + start + "-" + stop + " -i").read().split("\n")
-                        seq = "".join(output[1:])
-                    if strand=="+":
-                        output = os.popen("samtools faidx " + genome_file + " " +chrm + ":" + start + "-" + stop).read().split("\n")
-                        seq = "".join(output[1:])
-                    if not cds_dict:
-                        cds_dict[gene_id] = {'chrm':chrm,'strand':strand,'transcripts':{transcript_id:[{'exon':exon,'coordinates':(start,stop),'seq':seq}]}}
-                    else:
-                        if gene_id in cds_dict.keys():
-                            if transcript_id not in cds_dict[gene_id]['transcripts'].keys():
-                                cds_dict[gene_id]['transcripts'].update({transcript_id:[{'exon':exon,'coordinates':(start,stop),'seq':seq}]})
-                            else:
-                                cds_dict[gene_id]['transcripts'][transcript_id].append({'exon':exon,'coordinates':(start,stop),'seq':seq})
-                        else:
+                    gene_bio_type = attributes.split("gene_biotype")[1].split(";")[0]
+                    if gene_bio_type == "protein_coding":
+                        if strand=="-":                            
+                            output = os.popen("samtools faidx " + genome_file + " " +chrm + ":" + start + "-" + stop + " -i").read().split("\n")
+                            seq = "".join(output[1:])
+                        if strand=="+":
+                            output = os.popen("samtools faidx " + genome_file + " " +chrm + ":" + start + "-" + stop).read().split("\n")
+                            seq = "".join(output[1:])
+                        if not cds_dict:
                             cds_dict[gene_id] = {'chrm':chrm,'strand':strand,'transcripts':{transcript_id:[{'exon':exon,'coordinates':(start,stop),'seq':seq}]}}
+                        else:
+                            if gene_id in cds_dict.keys():
+                                if transcript_id not in cds_dict[gene_id]['transcripts'].keys():
+                                    cds_dict[gene_id]['transcripts'].update({transcript_id:[{'exon':exon,'coordinates':(start,stop),'seq':seq}]})
+                                else:
+                                    cds_dict[gene_id]['transcripts'][transcript_id].append({'exon':exon,'coordinates':(start,stop),'seq':seq})
+                            else:
+                                cds_dict[gene_id] = {'chrm':chrm,'strand':strand,'transcripts':{transcript_id:[{'exon':exon,'coordinates':(start,stop),'seq':seq}]}}
 
-    print(cds_dict)
+    #print(cds_dict)
     return cds_dict
 
 def get_single_transcript_cds(cds_dict,transcript_id):
